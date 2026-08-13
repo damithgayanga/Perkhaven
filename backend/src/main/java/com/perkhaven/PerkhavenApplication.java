c1be2f77be6d7db140576ec59c6fa9fb96bfcdf2
Venuka Fonseka <venuka@Venukas-MacBook-Air.local>
2026-08-14 09:54:35 +1200

    Ensure Fargate migration tasks exit

--- backend/src/main/java/com/perkhaven/PerkhavenApplication.java
+++ backend/src/main/java/com/perkhaven/PerkhavenApplication.java
@@ -2,14 +2,16 @@ package com.perkhaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@EnableScheduling
 @SpringBootApplication
 public class PerkhavenApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PerkhavenApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(PerkhavenApplication.class, args);
+        if (context.getEnvironment().getProperty("perkhaven.migration-only", Boolean.class, false)) {
+            System.exit(SpringApplication.exit(context));
+        }
     }
 }
